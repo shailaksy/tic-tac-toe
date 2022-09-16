a53c186ab3f0735c967dbc7ab8ca377ce01888b8
shailaksy <shailaksy@gmail.com>
2022-09-16 22:26:04 +0800

    updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,215 +1,246 @@
-const playerOneNameInput = document.querySelector('[data-input-player-one-name]');
-const playerOneName = document.querySelector('#player-one');
-const playerTwoNameInput = document.querySelector('[data-input-player-two-name]');
-const playerTwoName = document.querySelector('#player-two');
-const playerPage = document.querySelector('#player-page');
-const scoreboard = document.querySelector('#scoreboard');
-const playerOneNameDisplay = document.querySelector('#player-one-name');
-const playerTwoNameDisplay = document.querySelector('#player-two-name');
-const playerOneScoreDisplay = document.querySelector('#player-one-score');
-const playerTwoScoreDisplay = document.querySelector('#player-two-score');
+const playerOneNameInput = document.querySelector(
+  "[data-input-player-one-name]"
+);
+const playerOneName = document.querySelector("#player-one");
+const playerTwoNameInput = document.querySelector(
+  "[data-input-player-two-name]"
+);
+const playerTwoName = document.querySelector("#player-two");
+const playerPage = document.querySelector("#player-page");
+const scoreboard = document.querySelector("#scoreboard");
+const playerOneNameDisplay = document.querySelector("#player-one-name");
+const playerTwoNameDisplay = document.querySelector("#player-two-name");
+const playerOneScoreDisplay = document.querySelector("#player-one-score");
+const playerTwoScoreDisplay = document.querySelector("#player-two-score");
 
-const turnDisplay = document.querySelector('#turn-display-text');
+const turnDisplay = document.querySelector("#turn-display-text");
 
 playerTwoName.style.display = "none";
 turnDisplay.style.display = "none";
 
-playerOneNameInput.addEventListener('keyup', function(event) {
-    if (event.key === 'Enter') {
-        if (playerOneNameInput.value) {
-            playerOneName.style.display = "none";
-            playerTwoName.style.display = "block";
-            localStorage.setItem('PlayerOne', playerOneNameInput.value);
-            localStorage.PlayerOneScore = 0;
-        } else {
-            alert("Enter a new player\'s name.");
-        }
-    } 
-})
-
-playerTwoNameInput.addEventListener('keyup', function(event) {
-    if (event.key === 'Enter') {
-        if (playerTwoNameInput.value) {
-            if (playerTwoNameInput.value === playerOneNameInput.value) {
-                alert("Enter a new player\'s name.");
-            } else {
-                localStorage.setItem('PlayerTwo', playerTwoNameInput.value); 
-                localStorage.PlayerTwoScore = 0;
-                playerTwoName.style.display = "none";
-                scoreboard.style.display = "flex";
-                playerOneNameDisplay.textContent = playerOneNameInput.value;
-                playerTwoNameDisplay.textContent = playerTwoNameInput.value;
-                playerOneScoreDisplay.textContent = '0';
-                playerTwoScoreDisplay.textContent = '0';
-                playerPage.style.display = "none";
-                turnDisplay.style.display = "block";
-                turnDisplay.textContent = `${playerOneNameInput.value}'s turn`;
-                startGame();
-            }
-        } else {
-            alert("Enter a new player\'s name.");
-        }    
+playerOneNameInput.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    if (playerOneNameInput.value) {
+      playerOneName.style.display = "none";
+      playerTwoName.style.display = "block";
+      localStorage.setItem("PlayerOne", playerOneNameInput.value);
+      localStorage.PlayerOneScore = 0;
+    } else {
+      alert("Enter a new player's name.");
     }
-})
+  }
+});
+
+playerTwoNameInput.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    if (playerTwoNameInput.value) {
+      if (playerTwoNameInput.value === playerOneNameInput.value) {
+        alert("Enter a new player's name.");
+      } else {
+        localStorage.setItem("PlayerTwo", playerTwoNameInput.value);
+        localStorage.PlayerTwoScore = 0;
+        playerTwoName.style.display = "none";
+        scoreboard.style.display = "flex";
+        playerOneNameDisplay.textContent = playerOneNameInput.value;
+        playerTwoNameDisplay.textContent = playerTwoNameInput.value;
+        playerOneScoreDisplay.textContent = "0";
+        playerTwoScoreDisplay.textContent = "0";
+        playerPage.style.display = "none";
+        turnDisplay.style.display = "block";
+        turnDisplay.textContent = `${playerOneNameInput.value}'s turn`;
+        startGame();
+      }
+    } else {
+      alert("Enter a new player's name.");
+    }
+  }
+});
 
 ///
 
-const cells = document.querySelectorAll('.cell');
+const cells = document.querySelectorAll(".cell");
 
-const resetButton = document.querySelector('#reset');
-const restartButton = document.querySelector('#restart');
-const undoButton = document.querySelector('#undo');
-const redoButton = document.querySelector('#redo');
+const restartButton = document.querySelector("#restart");
+const backButton = document.querySelector("#back");
+const undoButton = document.querySelector("#undo");
+const redoButton = document.querySelector("#redo");
 
-const winnerDisplay = document.getElementById('winner-message');
-const winnerDisplayText = document.querySelector('[data-winner-message-text]');
+const winnerDisplay = document.getElementById("winner-message");
+const winnerDisplayText = document.querySelector("[data-winner-message-text]");
 
 const winCombos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
 ];
 
-let options = ["", "", "", "", "", "", "", "", ""];
+let placeholders = ["", "", "", "", "", "", "", "", ""];
 let currentPlayer = "X";
-let currentPlayerName = '';
-let running = false;
+let currentPlayerName = "";
+let gameRunning = false;
 
 let moves = 0;
 
+let boardData = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+];
+
+let movesHistory = [];
+
+let history = [
+  [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ],
+];
+
+let player = 1;
+
 function startGame() {
-    cells.forEach(cell => cell.addEventListener("click", cellClicked));
-    resetButton.addEventListener("click", restartGame);
-    running = true;
-}   
+  cells.forEach((cell) => cell.addEventListener("click", cellClicked));
+  restartButton.addEventListener("click", restartGame);
+  gameRunning = true;
+  turnDisplay.style.display = 'block';
+  undoButton.style.display = 'none';
+  redoButton.style.display = 'none';
+}
 
 function cellClicked() {
-    const cellIndex = this.getAttribute('cellIndex');
+  const cellIndex = this.getAttribute("cellIndex");
 
-    if (options[cellIndex] != "" || !running) { 
-        return;
-    }
-    
-    updateCell(this, cellIndex);
-    checkWinner();
+  if (placeholders[cellIndex] != "" || !gameRunning) {
+    return;
+  }
+
+  updateCell(this, cellIndex);
+  checkWinner();
 }
 
 function updateCell(cell, index) {
-    options[index] = currentPlayer;
-    cell.textContent = currentPlayer;
-    moves += 1;
-    console.log(moves);
+  placeholders[index] = currentPlayer;
+  cell.textContent = currentPlayer;
+  saveData(index);
 }
 
-
-/* hover
-
-    cell.addEventListener('mouseover', () => {
-        if (cell.textContent = '') {
-            if (moves % 2 === 0) {
-                cell.textContent = 'O';
-            } else {
-                cell.textContent = 'X';
-            }
-        }
-    })
-*/
-
 function changePlayer() {
-    currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
-    currentPlayerName = (currentPlayer === 'X') ? playerOneNameInput.value : playerTwoNameInput.value;
-    turnDisplay.textContent = `${currentPlayerName}'s turn`;
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
+  currentPlayerName =
+    currentPlayer === "X" ? playerOneNameInput.value : playerTwoNameInput.value;
+  turnDisplay.textContent = `${currentPlayerName}'s turn`;
 }
 
 function checkWinner() {
-    let roundWon = false;
+  let gameoverWin = false;
 
-    for(let i = 0; i < winCombos.length; i++) {
-        const combo = winCombos[i];
-        const cellA = options[combo[0]];
-        const cellB = options[combo[1]];
-        const cellC = options[combo[2]];
+  for (let i = 0; i < winCombos.length; i++) {
+    const combo = winCombos[i];
+    const cellRowOne = placeholders[combo[0]];
+    const cellRowTwo = placeholders[combo[1]];
+    const cellRowThree = placeholders[combo[2]];
 
-        if (cellA === '' || cellB === '' || cellC === '') {
-            continue;
-        } else if (cellA === cellB && cellB === cellC) {
-            roundWon = true;
-            break;
-        }
+    if (cellRowOne === "" || cellRowTwo === "" || cellRowThree === "") {
+      continue;
+    } else if (cellRowOne === cellRowTwo && cellRowTwo === cellRowThree) {
+      gameoverWin = true;
+      break;
     }
+  }
 
-    if(roundWon) {
-        winnerDisplay.style.display = 'flex';
-        winnerDisplayText.textContent = `${currentPlayerName} wins`;
-        running = false;
-        if (currentPlayer === 'X'){
-            localStorage.PlayerOneScore = Number(localStorage.PlayerOneScore) + 1;
-            playerOneScoreDisplay.textContent = localStorage.getItem('PlayerOneScore');
-        } else {
-            localStorage.PlayerTwoScore = Number(localStorage.PlayerTwoScore) + 1;
-            playerTwoScoreDisplay.textContent = localStorage.getItem('PlayerTwoScore');
-        }
-    } else if (!options.includes('')) {
-        winnerDisplay.style.display = 'flex';
-        winnerDisplayText.textContent = `draw`;
-        running = false;
+  if (gameoverWin) {
+    winnerDisplay.style.display = "flex";
+    winnerDisplayText.textContent = `${currentPlayerName} wins`;
+    turnDisplay.style.display = 'none';
+    gameRunning = false;
+    if (currentPlayer === "X") {
+      localStorage.PlayerOneScore = Number(localStorage.PlayerOneScore) + 1;
+      playerOneScoreDisplay.textContent =
+        localStorage.getItem("PlayerOneScore");
     } else {
-        changePlayer();
+      localStorage.PlayerTwoScore = Number(localStorage.PlayerTwoScore) + 1;
+      playerTwoScoreDisplay.textContent =
+        localStorage.getItem("PlayerTwoScore");
     }
+  } else if (!placeholders.includes("")) {
+    winnerDisplay.style.display = "flex";
+    winnerDisplayText.textContent = `draw`;
+    gameRunning = false;
+  } else {
+    changePlayer();
+  }
 }
 
 function restartGame() {
-    currentPlayer = 'X';
-    options = ['', '', '', '', '', '', '', '', ''];
-    turnDisplay.textContent = `${playerOneNameInput.value}'s turn`;
-    cells.forEach(cell => cell.textContent = '');
-    running = true;
+  currentPlayer = "X";
+  placeholders = ["", "", "", "", "", "", "", "", ""];
+  moves = 0;
+  movesHistory = [];
+  boardData = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  player = 1;
+  turnDisplay.style.display = 'block';
+  undoButton.style.display = 'none';
+  redoButton.style.display = 'none';
+  localStorage.removeItem("MovesHistory");
+  turnDisplay.textContent = `${playerOneNameInput.value}'s turn`;
+  cells.forEach((cell) => (cell.textContent = ""));
+  gameRunning = true;
 }
 
-restartButton.addEventListener("click", () => {
-    restartGame();
-    winnerDisplay.style.display = 'none';
+backButton.addEventListener("click", () => {
+  winnerDisplay.style.display = "none";
+  undoButton.style.display = 'inline';
+  redoButton.style.display = 'inline';
 });
 
-
-///
-
-/*
-if() // if board has cells, allow undo
-
-undoButton.addEventListener('click', () => {
-
-})
-
-redoButton.addEventListener('click', () => {
-
-})
-
-function previousMove() {
-
+const displayBoard = (array) => {
+    array.flat().forEach((cell, i) => {
+        if (cell !== "") {
+            cells[i].textContent = cell > 0 ? "X" : "O";
+        } else {
+            cells[i].textContent = cell;
+        }
+    });
 }
 
-function nextMove() {
+function saveData(i) {
+  let col = i % 3;
+  let row = (i - col) / 3;
+  boardData[row][col] = player; //x
+  player *= -1; //o
+  moves++;
 
+  movesHistory.push(JSON.parse(JSON.stringify(boardData)));
+  localStorage.setItem("MovesHistory", movesHistory);
+
+  history[moves] = JSON.parse(JSON.stringify(boardData));
+  localStorage.setItem("History", history);
 }
-*/
 
-const array = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
-];
+function undoMove() {
+    if (moves > 1) {
+        history.pop();
+        moves--;
+        displayBoard(history[moves]);
+    } else {
+        undoButton.style.display = 'none';
+    }
+}
 
-const history = [
-    [
-        ["", "", ""],
-        ["", "", ""],
-        ["", "", ""],
-    ],
-];
+function redoMove() {
+    moves++;
+    console.log(history[moves]);
+}
 
+undoButton.addEventListener('click', undoMove);
+redoButton.addEventListener('click', redoMove);
